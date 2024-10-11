1e0d56e38a911b6890f786a426db0926008a3e0d
Meet211003 <163899897+Meet211003@users.noreply.github.com>
2024-10-11 16:01:18 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "dmcgmchdcjhzveuovppb.supabase.co"
+        ]
+    }
+};
+
+export default nextConfig;
